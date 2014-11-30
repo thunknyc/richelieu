52467304a6cda51bae0f4b0991ff7dd84092cdc6
Edwin Watkeys <edw@poseur.com>
2014-11-30 11:28:21 -0500

    fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject thunknyc/richelieu "0.1.0-SNAPSHOT"
   :description "A Clojure advice library"
-  :url "http://github/thunknyc/richelieu"
+  :url "http://github.com/thunknyc/richelieu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
